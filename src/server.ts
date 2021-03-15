ee19438655f98abc5e0dcf03785ed119adf069cd
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-15 18:55:50 -0300

    Subindo alterações

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { createCourse } from "./routes";
+
+const app = express();
+
+app.get("/", createCourse);
+
+app.listen(3333, () => {
+    console.log("Server is running!");
+});
